d43cb9d053e85ae215e2ffe9e02fa48e583d4f85
gent20 <gentkastratio@gmail.com>
2021-03-12 13:56:41 +0100

    Tested getBeerById method with JsonPath

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -1,14 +1,17 @@
 package guru.springframework.brewery.web.controllers;
 
+import com.jayway.jsonpath.JsonPath;
 import guru.springframework.brewery.domain.Beer;
 import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
 import guru.springframework.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -16,7 +19,12 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class BeerControllerTest {
@@ -46,4 +54,16 @@ class BeerControllerTest {
 
         mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
     }
+
+    @Test
+    void testGetBeerById() throws Exception {
+        given(beerService.findBeerById(any())).willReturn(validBeer);
+
+        mockMvc.perform(get("/api/v1/beer/" + validBeer.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id",is(validBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName",is(validBeer.getBeerName())));
+    }
+
 }
